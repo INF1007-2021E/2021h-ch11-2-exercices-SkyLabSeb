1d745331691358d77f50333d04b6a5a15cdf717a
SkyLabSeb <77995832+SkyLabSeb@users.noreply.github.com>
2021-04-13 14:33:07 -0400

    update

--- game.py
+++ game.py
@@ -13,7 +13,13 @@ from magician import *
 
 
 def deal_damage(attacker: Character, defender: Character):
-	pass
+	weapon_used = attacker.spell if isinstance(attacker, Magician) and attacker.will_use_spell() else attacker.weapon
+	damage, crit = attacker.compute_damage(defender)
+	defender.hp -= damage
+	print(f"{attacker.name} used {weapon_used.name}")
+	if crit:
+		print("  Critical hit!")
+	print(f"  {defender.name} took {damage} dmg")
 
 def run_battle(c1: Character, c2: Character):
 	pass
